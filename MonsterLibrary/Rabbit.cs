866c85367a7bb8524b5225b4fec275d2a076770b
Brandon Buck <brandonbuck96@outlook.com>
2022-04-16 14:50:22 -0500

    Initial Commit

---
+++ MonsterLibrary/Rabbit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonLibrary; //Added for access to the Monster class
+
+namespace MonsterLibrary
+{
+    public class Rabbit : Monster
+    {
+        //Created this class as a CHILD class of Monster
+        //It will extend the info in Monster with its own unique properties.
+
+        public bool IsFluffy { get; set; }
+
+        public Rabbit(string name, int life, int maxLife, int hitChance, int block, int maxDamage, string description, int minDamage, bool isFluffy) : base(name, life, maxLife, hitChance, block, maxDamage, description, minDamage)
+        {
+            IsFluffy = isFluffy;
+
+        }
+
+        //This is a CTOR that creates a Rabbit object with default values.
+        public Rabbit()
+        {
+            MaxLife = 6;
+            MaxDamage = 3;
+            //The above 2 are set first because other properties depend on them for their business rules.
+            Name = "Baby Rabbit";
+            Life = 6;
+            HitChance = 20;
+            Block = 20;
+            MinDamage = 1;
+            Description = "It's just a cute baby bunny...why would you hurt it?";
+            IsFluffy = false;
+        }
+
+        //METHODS
+
+        public override string ToString()
+        {
+            return base.ToString() + (IsFluffy ? "Fluffiness: Fluffy" : "Fluffiness: Not Fluffy");
+        }
+
+        //We will override CalcBlock() to give a 50% increase if fluffy
+        public override int CalcBlock()
+        {
+            int calculatedBlock = Block;
+
+            if (IsFluffy)
+            {
+                calculatedBlock += calculatedBlock / 2;
+
+            }
+
+            return calculatedBlock;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DungeonLibrary;
+using MonsterLibrary;
 
 namespace DungeonApp
 {
@@ -10,6 +12,153 @@ namespace DungeonApp
     {
         static void Main(string[] args)
         {
+            Console.Title = "DUNGEON OF DOOM!";
+
+            Console.WriteLine("Your journey begins...\n");
+            // 1 Create a Player & a Weapon
+
+            Weapon sword = new Weapon("Falchion", false, 1, 5, 10);
+            Player player = new Player("Garbok", 5, 30, 100, 100, Race.Tiefling, sword);
+            int score = 0;
+
+
+            //2. Create a Loop for the game
+
+            bool exit = false;
+
+            do
+            {
+                //3. Create a room - making a method to generate a room description.
+                Console.WriteLine("You have now entered a room...\n");
+                Console.WriteLine(GetRoom());
+
+                // 4. Create a Monster
+                Rabbit r1 = new Rabbit(); //This one uses the default values
+                Rabbit r2 = new Rabbit("White Rabbit", 25, 25, 50, 20, 8, "That's no ordinary rabbit, look at the bones!", 2, true);
+
+                //Since all of our monsters are the same type, we will create an array with a few instances of each rabbit above.
+                //In your dungeon, put one instance of each kind of monster in the array.
+
+                Monster[] monsters = { r1, r2, r1, r1, r2, r2, r1 };
+
+                //Randomly select a monster from the array.
+                Random rand = new Random();
+
+                int randNbr = rand.Next(monsters.Length);
+                Monster monster = monsters[randNbr];
+
+                Console.WriteLine("\nIn this room: " + monster.Name);
+
+
+                //5. Create a Loop for the Menu
+                bool reload = false;
+                do
+                {
+                    //6. Provide the User with a Menu of Options
+                    Console.WriteLine("\nPlease choose an action\n" +
+                        "A) Attack\n" +
+                        "R) Run Away\n" +
+                        "P) Player Info\n" +
+                        "W) Weapon Stats\n" +
+                        "M) Monster Info\n" +
+                        "X) Exit\n");
+
+                    //7. Catch the user's choice
+                    //string userChoice = Console.ReadLine().ToUpper(); ---OPTION 1
+                    ConsoleKey userChoice = Console.ReadKey(true).Key; // -----OPTION 2
+
+                    Console.Clear();
+
+                    //8. Build out a switch to handle the user's choice.
+                    switch (userChoice)
+                    {
+                        case ConsoleKey.A:
+                            // 9 Handle the combat logic & what happens if the player wins
+                            Combat.DoBattle(player, monster);
+                            if (monster.Life <= 0)
+                            {
+                                //it's dead
+                                //Possible customization, player could get items, life, or something similar
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("\nYou killed {0}\n", monster.Name);
+                                Console.ResetColor();
+                                //get a new room and new monster
+                                reload = true; //Breaks out of interior loop.
+                                //Update the players score. 
+                                score++;
+                            }
+
+                            break;
+                        case ConsoleKey.R:
+                            Console.WriteLine("Run Away!!!");
+                            //TODO 10 Handle the user running away & the monster getting a free attack
+                            Console.WriteLine($"{monster.Name} attacks as you flee!");
+                            Combat.DoAttack(monster, player); //free attack, only the monster gets to attack here.
+                            Console.WriteLine();
+                            reload = true; //Get a new room and new monster, because player left the previous.
+
+
+                            break;
+                        case ConsoleKey.P:
+                            Console.WriteLine("Player Info: ");
+                            // 11 Print out Player info
+                            Console.WriteLine(player);
+                            break;
+                        case ConsoleKey.W:
+                            Console.WriteLine(sword);
+                            break;
+                        case ConsoleKey.M:
+                            Console.WriteLine("Monster Info: ");
+                            // 12 Print out Monster info
+                            Console.WriteLine(monster);
+                            break;
+                        case ConsoleKey.X:
+                        case ConsoleKey.E:
+                            Console.WriteLine("Nobody likes a quitter! Boo!");
+                            exit = true;
+                            break;
+
+                        default:
+                            Console.WriteLine("Thou hast chosen an improper option. Triest though again.");
+                            break;
+                    }
+
+                    // 13 Check the Player's life (health)
+                    if (player.Life <= 0)
+                    {
+                        Console.WriteLine("Dude...you are already dead.");
+                        exit = true;
+                    }
+
+                } while (!exit && !reload); //While exit AND reload are false, keep running
+
+            } while (!exit);//While exit is false, keep running
+
+            // 14 State the Player's Score (How many monsters were killed?)
+            Console.WriteLine("Congrats! You slayed {0} monster{1}!", score, ((score == 1) ? "" : "s"));
+        }
+        private static string GetRoom()
+        {
+            string[] rooms =  {
+                "The room is dark and musty with the smell of lost souls.",
+                "You enter a pretty pink powder room and instantly get glitter on you.",
+                "You arrive in a room filled with chairs and a ticket stub machine...DMV",
+                "You enter a quiet library... silence... nothing but silence....",
+                "As you enter the room, you realize you are standing on a platform surrounded by sharks",
+                "Oh my.... what is that smell? You appear to be standing in a compost pile",
+                "You enter a dark room and all you can hear is hair band music blaring.... This is going to be bad!",
+                "Oh no.... the worst of them all... Oprah's bedroom....",
+                "The room looks just like the room you are sitting in right now... or does it?",
+            };
+
+            Random rand = new Random();
+            int indexNbr = rand.Next(rooms.Length);
+            string room = rooms[indexNbr];
+            return room;
+
         }
     }
+
 }
+
+
